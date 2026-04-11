b149f6ae6517fa437dab9a2c1c9cb414a9a11141
borogk <tyborogk@gmail.com>
2026-04-12 01:56:31 +0200

    LSP and autocomplete setup

--- init.lua
+++ init.lua
@@ -4,10 +4,23 @@ vim.o.termguicolors = true
 vim.o.number = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
-vim.cmd.colorscheme('tokyonight-night')
+vim.opt.completeopt = { "menuone", "popup", "noinsert" }
+vim.cmd.colorscheme("tokyonight-night")
 
-vim.keymap.set('n', '|', '<cmd>Neotree<cr>')
-vim.keymap.set('n', '<C-t>', '<cmd>Telescope<cr>')
-vim.keymap.set('n', '<C-n>', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<C-g>', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set("n", "|", "<cmd>Neotree<cr>")
+vim.keymap.set("n", "<C-t>", "<cmd>Telescope<cr>")
+vim.keymap.set("n", "<C-n>", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<C-g>", "<cmd>Telescope live_grep<cr>")
+vim.keymap.set("n", "<C-l>", vim.lsp.buf.format)
 
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+		if client:supports_method("textDocument/completion") then
+			vim.lsp.completion.enable(true, client.id, args.buf, {
+				autotrigger = true,
+			})
+		end
+	end,
+})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
-    "nvim-telescope/telescope.nvim", version = "*",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    }
+	"nvim-telescope/telescope.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 }
