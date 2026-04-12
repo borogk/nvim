55afa7f3cafcbde30f4d637af70f2dad80c4f55f
borogk <tyborogk@gmail.com>
2026-04-12 16:12:14 +0200

    Better keyboard shortcuts, better clipboard

--- init.lua
+++ init.lua
@@ -4,16 +4,37 @@ vim.o.termguicolors = true
 vim.o.number = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.o.clipboard = "unnamedplus"
 vim.opt.completeopt = { "menuone", "popup", "noinsert" }
 vim.cmd.colorscheme("tokyonight-night")
 
-vim.keymap.set("n", "|", "<cmd>Neotree<cr>")
-vim.keymap.set("n", "<C-t>", "<cmd>Telescope<cr>")
-vim.keymap.set("n", "<C-n>", "<cmd>Telescope find_files<cr>")
-vim.keymap.set("n", "<C-g>", "<cmd>Telescope live_grep<cr>")
-vim.keymap.set("n", "<C-l>", vim.lsp.buf.format)
-vim.keymap.set("n", "<F12>", "<cmd>Telescope lsp_document_symbols<cr>")
+vim.keymap.set({ "n", "i", "v" }, "<C-t>", "<cmd>Telescope<cr>")
+vim.keymap.set({ "n", "i", "v" }, "<C-n>", "<cmd>Telescope find_files<cr>")
+vim.keymap.set({ "n", "i", "v" }, "<C-l>", vim.lsp.buf.format)
+vim.keymap.set({ "n", "i", "v" }, "<F18>", vim.lsp.buf.rename)
+vim.keymap.set({ "n", "i", "v" }, "<F19>", "<cmd>Telescope lsp_references<cr>")
 
+-- Open neotree, select current file if it exists, otherwise select working directory
+vim.keymap.set("n", "<C-/>", function()
+	local reveal_file = vim.fn.expand("%:p")
+	if reveal_file == "" then
+		reveal_file = vim.fn.getcwd()
+	else
+		local f = io.open(reveal_file, "r")
+		if f then
+			f.close(f)
+		else
+			reveal_file = vim.fn.getcwd()
+		end
+	end
+	require("neo-tree.command").execute({
+		toggle = true,
+		reveal_file = reveal_file,
+		reveal_force_cwd = true,
+	})
+end)
+
+-- Setup autocompletion
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
