71c922b94e985b59c3896f4881dc1eb7766684ea
borogk <tyborogk@gmail.com>
2026-04-18 13:38:53 +0200

    Split direction fix

--- init.lua
+++ init.lua
@@ -5,6 +5,8 @@ vim.o.number = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.clipboard = "unnamedplus"
+vim.o.splitbelow = true
+vim.o.splitright = true
 vim.opt.completeopt = { "menuone", "popup", "noinsert" }
 vim.cmd.colorscheme("tokyonight-night")
 
