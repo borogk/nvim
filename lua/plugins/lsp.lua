ada9def18ec7f81fd2b9cc8c985bd8bdccce2f51
borogk <tyborogk@gmail.com>
2026-04-18 23:07:47 +0200

    Ensure certain LSPs installed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,15 @@
 return {
 	{ "neovim/nvim-lspconfig" },
 	{ "mason-org/mason.nvim", opts = {} },
-	{ "mason-org/mason-lspconfig.nvim", opts = {} },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"clangd",
+				"gopls",
+				"shfmt",
+			},
+		},
+	},
 }
