7ac1a2a6dd5dd41a8f06fbae94d2b899e2258a30
borogk <tyborogk@gmail.com>
2026-04-18 13:09:54 +0200

    Anti-conceal disabled for Markdown, redesigned keymaps

--- lua/plugins/md.lua
+++ lua/plugins/md.lua
@@ -1,5 +1,7 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
-	opts = {},
+	opts = {
+		anti_conceal = { enabled = false },
+	},
 }
