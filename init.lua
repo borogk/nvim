38f830dd1a46f8e130100dede4b2ff24714d2ccf
borogk <tyborogk@gmail.com>
2026-04-18 20:51:37 +0200

    Enable dynamic title

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ vim.o.shiftwidth = 2
 vim.o.clipboard = "unnamedplus"
 vim.o.splitbelow = true
 vim.o.splitright = true
+vim.o.title = true
 vim.opt.completeopt = { "menuone", "popup", "noinsert" }
 vim.cmd.colorscheme("tokyonight-moon")
 
