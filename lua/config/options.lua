0b53ccace41bc5054b22a320d8b85724d5907525
borogk <tyborogk@gmail.com>
2026-05-01 01:46:20 +0200

    Display tabs and trailing spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,7 @@ vim.o.number = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.o.list = true
 vim.o.clipboard = "unnamedplus"
 vim.o.splitbelow = true
 vim.o.splitright = true
