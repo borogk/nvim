265f3cc8251dd915b73b53d4041fc2430e1168ba
borogk <tyborogk@gmail.com>
2026-05-01 17:19:05 +0200

    Improved quick menu

--- lua/config/quickaction.lua
+++ lua/config/quickaction.lua
@@ -10,17 +10,37 @@ local regular_items = {
     "switch_session",
     "recent",
     "clear_highlight",
+    "undo_history",
     "toggle_explorer",
     "toggle_wrap",
     "toggle_non_printables",
     "quit",
 }
 
+local function pluralize_noun(count, noun, ending)
+    if count == 1 then
+        return tostring(count) .. " " .. noun
+    else
+        return tostring(count) .. " " .. noun .. ending
+    end
+end
+
+local function enabled_text(enabled)
+    if enabled then
+        return "ON"
+    else
+        return "off"
+    end
+end
+
 function QuickAction(startup)
     local items = regular_items
     if startup == true then
         items = startup_items
     end
+
+    local wrap_enabled = vim.o.wrap
+    local non_printables_enabled = vim.o.list
 
     Snacks.picker.select(items, {
         prompt = "Quick action:",
@@ -34,19 +54,21 @@ function QuickAction(startup)
                         num_buffers = num_buffers + 1
                     end
                 end
-                return "Save session (" .. tostring(num_buffers) .. " buffer(s), " .. vim.fn.getcwd() .. ")"
+                return "Save session (" .. vim.fn.getcwd() .. ", " .. pluralize_noun(num_buffers, "buffer", "s") .. ")"
             elseif item == "switch_session" then
                 return "Switch session..."
             elseif item == "recent" then
                 return "Recent files..."
             elseif item == "clear_highlight" then
                 return "Clear highlight"
+            elseif item == "undo_history" then
+                return "Undo history..."
             elseif item == "toggle_explorer" then
                 return "Toggle explorer panel"
             elseif item == "toggle_wrap" then
-                return "Toggle wrap"
+                return "Toggle wrap (currently " .. enabled_text(wrap_enabled) .. ")"
             elseif item == "toggle_non_printables" then
-                return "Toggle non-printable characters"
+                return "Toggle non-printable characters (currently " .. enabled_text(non_printables_enabled) .. ")"
             elseif item == "quit" then
                 return "Quit"
             else
@@ -64,6 +86,8 @@ function QuickAction(startup)
             Snacks.picker.recent()
         elseif item == "clear_highlight" then
             vim.cmd("noh")
+        elseif item == "undo_history" then
+            Snacks.picker.undo()
         elseif item == "toggle_explorer" then
             Snacks.explorer({
                 auto_close = false,
